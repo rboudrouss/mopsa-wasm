ae693f271b5c9e3632ae2095b8387c28bdc9bfab
rboudrouss <r.boudrouss@gmail.com>
2025-04-28 21:08:07 +0200

    wasm stdlibs

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ rm -f build/*
 
 cd ocaml-wasm
 
+make clean
+
 CFLAGS="-fPIC -g0" emconfigure ./configure
 
 cd runtime
@@ -12,13 +14,15 @@ emmake make
 
 cd ../..
 
-# Building mopsa to bytecode (ocaml >= 4.12)
+# Building mopsa to bytecode (ocaml == 4.12.0)
 
 cd mopsa-analyzer-js
 
 if [ "$1" = "-i" ]; then # install only if given -i
   opam install --deps-only .
 fi
+
+make clean
 
 ./configure
 
@@ -39,16 +43,16 @@ gcc -c -o build/floats_round.o mopsa-analyzer-js/utils/itvUtils/floats_round.c -
 ocamlc -c -o build/floats_round.cmo wrappers/floats_round.ml
 ocamlc -o build/floats_round.bc build/floats_round.o build/floats_round.cmo
 
-emcc -o build/ocamlrun.html ocaml-wasm/runtime/prims.o ocaml-wasm/runtime/libcamlrun.a \
-  -L./build -l:libmopsa_c_parser_stubs.a -l:libitvUtils_stubs.a \
+emcc -o build/ocamlrun.html ocaml-wasm/runtime/prims.o ocaml-wasm/runtime/libcamlrun.a libs/dllcamlstr.wasm \
   -s WASM=1  -s ALLOW_MEMORY_GROWTH=1 \
   -s EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'FS', 'run','callMain']" \
   -s MAIN_MODULE=1 -s NO_EXIT_RUNTIME=1 \
-  -s FORCE_FILESYSTEM=1 \
+  -s FORCE_FILESYSTEM=1 -s SINGLE_FILE=1 -s WASM_ASYNC_COMPILATION=0\
   -s ENVIRONMENT='web'  --preload-file build/mopsa.bc \
-  --preload-file build/dllmopsa_c_parser_stubs.so@lib/dllmopsa_c_parser_stubs.so \
-  --preload-file build/dllitvUtils_stubs.so@lib/dllitvUtils_stubs.so \
   --pre-js prejs.js
+  # --preload-file build/dllmopsa_c_parser_stubs.so@lib/dllmopsa_c_parser_stubs.so \
+  # --preload-file build/dllitvUtils_stubs.so@lib/dllitvUtils_stubs.so \
+#  -L./build -l:libmopsa_c_parser_stubs.a -l:libitvUtils_stubs.a \
   #--preload-file build/floats_round.bc \
 #  -s EXPORTED_FUNCTIONS="['_malloc','_free','_dlopen','_dlsym','_dlclose','_main']" \
 # -lpthreads
---
+++ libs/README.md
@@ -0,0 +1,3 @@
+dllcamlstr, dllthreads, dllunix come from https://github.com/corwin-of-amber/ocaml-wasm/tree/wasm-4.12
+
+big thanks to him for the work done on this project.
