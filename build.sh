a2aa13b45390d85207532b893a75c47e3f8f468e
rboudrouss <r.boudrouss@gmail.com>
2025-04-28 22:04:22 +0200

    omg compiled intvutils & kinda clangtomk

--- build.sh
+++ build.sh
@@ -35,9 +35,26 @@ cd ..
 mkdir -p build 
 
 cp mopsa-analyzer-js/_build/default/analyzer/mopsa.bc build/
-cp mopsa-analyzer-js/_build/default/parsers/c/lib/parser/libmopsa_c_parser_stubs.a build/
-cp mopsa-analyzer-js/_build/default/utils/itvUtils/libitvUtils_stubs.a build/
-#cp mopsa-analyzer-js/_build/default/temp/mopsaJs.bc build/
+
+# build libitvUtils_stubs
+
+emcc -fno-strict-aliasing -fwrapv -fPIC -D_FILE_OFFSET_BITS=64 -D_REENTRANT -fdiagnostics-color=always -g -I ~/.opam/4.12.0/lib/ocaml -I ~/.opam/4.12.0/lib/zarith -I mopsa-analyser-js/_build/default/utils/core -o build/floats_round.o -c mopsa-analyzer-js/utils/itvUtils/floats_round.c
+
+emcc -g -o build/itvUtils_stubs build/floats_round.o
+
+# build libmopsa_c_parser_stubs
+
+cp -r /usr/include/llvm/ includes/llvm/
+cp -r /usr/include/clang/ includes/clang/
+cp -r /usr/include/llvm-c/ includes/llvm-c/
+
+emcc -x c++ -fno-strict-aliasing -fwrapv -fPIC -fdiagnostics-color=always -I ~/.opam/4.12.0/lib/ocaml -Wno-strict-aliasing -Wall -Wno-comment -std=c++17 -fno-exceptions -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS '-DCLANGRESOURCE="/usr/lib/clang/19"' -g -I ~/.opam/4.12.0/lib/ocaml -I ~/.opam/4.12.0/lib/zarith -I ./includes -o build/Clang_to_ml.o -c mopsa-analyzer-js/parsers/c/lib/parser/Clang_to_ml.cc
+
+emcc -g -o build/libmopsa_c_parser_stubs build/Clang_to_ml.o -L/usr/lib  -lclang-cpp -lclang -lstdc++ -lLLVM-19
+# we need libclang-cpp.so libclang.so libLLVM-19.so yeah clang compiling time
+
+
+cp libs/*.wasm build/
 
 gcc -c -o build/floats_round.o mopsa-analyzer-js/utils/itvUtils/floats_round.c -I $(ocamlc -where)
 ocamlc -c -o build/floats_round.cmo wrappers/floats_round.ml
@@ -47,7 +64,7 @@ emcc -o build/ocamlrun.html ocaml-wasm/runtime/prims.o ocaml-wasm/runtime/libcam
   -s WASM=1  -s ALLOW_MEMORY_GROWTH=1 \
   -s EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'FS', 'run','callMain']" \
   -s MAIN_MODULE=1 -s NO_EXIT_RUNTIME=1 \
-  -s FORCE_FILESYSTEM=1 -s SINGLE_FILE=1 -s WASM_ASYNC_COMPILATION=0\
+  -s FORCE_FILESYSTEM=1 -s SINGLE_FILE=1\
   -s ENVIRONMENT='web'  --preload-file build/mopsa.bc \
   --pre-js prejs.js
   # --preload-file build/dllmopsa_c_parser_stubs.so@lib/dllmopsa_c_parser_stubs.so \
--- libs/README.md
+++ libs/README.md
@@ -1,3 +1,3 @@
 dllcamlstr, dllthreads, dllunix come from https://github.com/corwin-of-amber/ocaml-wasm/tree/wasm-4.12
 
-big thanks to him for the work done on this project.
+big thanks to them for the work done on this project.
